269334db322d58512e98d230dab93bb1dfe9d8cd
ozzigit <ozzi@ukr.net>
2022-06-08 15:40:16 +0300

    folders

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import re
 import pathlib
 
 
-
 def valid_target_dir(path):
     path = pathlib.Path(path)
     try:
@@ -37,23 +36,39 @@ def init_args():
     return parser.parse_args()
     # print(parser.parse_args())
 
-def gerenerate_file_mask():
-    FileTypes = ('.jpg', '.mp4')
 
+def gerenerate_file_mask(fileName: str):
+    # FileTypes = ('.jpg', '.mp4')
+    if re.search(r'^\d{8}_\d{6}', fileName):
+        return True
 
 
 def generate_list_of_edit_files(directory: pathlib.Path):
     file_list = []  # A list for storing files existing in directories
     for x in directory.iterdir():
-        if x.is_file() and x.name :
+        if x.is_file() and gerenerate_file_mask(x.name):
             file_list.append(x)
         # else:
         # file_list.append(searching_all_files(directory / x))
     return file_list
 
 
-# Piress the green button in the gutter to run the script.
+def checkYearDir_orCreate(fileName):
+    dirName = fileName[:4]
+
+
+def checkMothDir_orCreate(fileName):
+    dirName = fileName[4:6]
+
+
 if __name__ == '__main__':
     args = init_args()
-    generate_list_of_edit_files(pathlib.Path(args.targetPath))
+    file_list = generate_list_of_edit_files(pathlib.Path(args.targetPath))
+    for fileName in file_list:
+        dist_dir = args.resultPath + checkYearDir_orCreate(fileName)
+        if args.withMonth in ('Yy'):
+            dist_dir += checkMothDir_orCreate(fileName)
+        pathlib.Path(args.targetPath).rename(dist_dir + fileName)
+        pass
+
     print(args.__dict__)
